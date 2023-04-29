7cf31826e4c63d33e616e75e66180aaab579bf74
Saidmurod Mukhitdinov <saidmurodmuhitdinov@gmail.com>
2023-04-29 10:52:59 -0600

    Add project files.

---
+++ Giraffe/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giraffe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string input = Console.ReadLine();
+            Console.WriteLine($"Hello {input}");
+            Console.ReadLine();
+        }
+    }
+}
